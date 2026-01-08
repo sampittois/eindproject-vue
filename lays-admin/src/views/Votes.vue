07e90b3a87f973350564991d8f425ec23ad8836f
sampittois <sam.pittois@gmail.com>
2026-01-08 10:12:24 +0100

    feat: create Votes component with template, script, and styles

---
+++ lays-admin/src/views/Votes.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Votes Page</h1>
+    <!-- Add your content here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Votes',
+  // Add your component logic here
+}
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
